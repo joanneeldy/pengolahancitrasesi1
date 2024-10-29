bff080a2b859bd9db94a5bee1c37a5ee7758c56d
Joanne LT <95136347+joanneeldy@users.noreply.github.com>
2024-10-29 22:23:51 +0700

    Refine channel.py

--- channel.py
+++ channel.py
@@ -1,35 +1,44 @@
-#pip install numpy
+# pip install numpy, imageio, matplotlib
 import numpy as np
 import imageio as img
 import matplotlib.pyplot as plt
 
-image = img.imread("D:/source.jpg")
+# Load image
+image = img.imread(r'C:\Users\LENOVO\Downloads\Pengolahan Citra Sesi 1\assets\source.jpg')
 
+# Split image into RGB components
 red = image[:,:,0]
 green = image[:,:,1]
 blue = image[:,:,2]
 
+# Create empty arrays for colored images
 imgRed = np.zeros_like(image)
 imgGreen = np.zeros_like(image)
 imgBlue = np.zeros_like(image)
 
+# Assign respective colors to their images
 imgRed[:,:,0] = red
 imgGreen[:,:,1] = green
 imgBlue[:,:,2] = blue
 
-plt.figure(figsize=(10,10))
-plt.subplot(3,1,1)
+# Plot the images
+plt.figure(figsize=(10, 10))
+
+plt.subplot(4, 1, 1)
+plt.title("Original Image")
 plt.imshow(image)
 
-plt.subplot(3,1,2)
+plt.subplot(4, 1, 2)
+plt.title("Red Channel")
 plt.imshow(imgRed)
 
-plt.subplot(3,1,3)
-plt.title("Green")
+plt.subplot(4, 1, 3)
+plt.title("Green Channel")
 plt.imshow(imgGreen)
 
-plt.subplot(3,1,4)
-plt.title("Blue")
+plt.subplot(4, 1, 4)
+plt.title("Blue Channel")
 plt.imshow(imgBlue)
 
+plt.tight_layout()
 plt.show()
